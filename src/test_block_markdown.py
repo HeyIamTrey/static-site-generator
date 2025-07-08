caccf0a33202759a8b5f7a27168c2bf81453b944
HeyIamTrey <tearnest3@gmail.com>
2025-07-08 10:33:53 -0400

    Created Block Types and a function that returns the block type based on an input block.

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from block_markdown import(
+    BlockType,
     markdown_to_blocks,
+    block_to_block_type
 )
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -80,6 +82,53 @@ This is block two
             blocks,
             [],
         )
+
+    def test_block_to_block_type_heading(self):
+        block1 = "# This is a heading"
+        block2 = "## This is also a heading"
+        block3 = "### This is also, also a heading"
+        block4 = "#### This is also, also, also, a heading"
+        block5 = "##### This is also, also, also, also a heading"
+        block6 = "###### This is also, also, also, also, also a heading"
+
+        block_type1 = block_to_block_type(block1)
+        block_type2 = block_to_block_type(block2)
+        block_type3 = block_to_block_type(block3)
+        block_type4 = block_to_block_type(block4)
+        block_type5 = block_to_block_type(block5)
+        block_type6 = block_to_block_type(block6)
+
+        self.assertEqual(block_type1, BlockType.HEADING)
+        self.assertEqual(block_type2, BlockType.HEADING)
+        self.assertEqual(block_type3, BlockType.HEADING)
+        self.assertEqual(block_type4, BlockType.HEADING)
+        self.assertEqual(block_type5, BlockType.HEADING)
+        self.assertEqual(block_type6, BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        block = "```\nThis is a block of code text\n```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+    
+    def test_block_to_block_type_quote(self):
+        block = "> This is a block\n> of quote text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_ulist(self):
+        block = "- This is a block\n- of unordered list text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ULIST)
+
+    def test_block_to_block_text_olist(self):
+        block = "1. This is a block\n2. of ordered list text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.OLIST)
+
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a block of paragraph text"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
